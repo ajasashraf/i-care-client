b05cc9bd166dd0f5346a8ab847d63c9fca566a43
Ajas Ashraf <ajasashraf7010@gmail.com>
2023-04-25 00:13:23 +0530

    url

--- client/api/apiLinks.js
+++ client/api/apiLinks.js
@@ -1,4 +1,10 @@
-export const userUrl = "http://localhost:2000/";
+// export const userUrl = "http://localhost:2000/";
+// export const doctorUrl = "http://localhost:2000/doctor/";
+// export const adminUrl = "http://localhost:2000/admin/";
+// export const chatUrl = "http://localhost:2000/chat/";
+// export const doctorChatUrl = "http://localhost:2000/doctor/chat/";
+
+export const userUrl = "https://i-care.tech/";
 export const doctorUrl = "http://localhost:2000/doctor/";
 export const adminUrl = "http://localhost:2000/admin/";
 export const chatUrl = "http://localhost:2000/chat/";
