2e690836ac408ab6ca63018587536c03e1d6e852
Ajas Ashraf <ajasashraf7010@gmail.com>
2023-04-25 19:01:38 +0530

    commit changes

--- client/api/apiLinks.js
+++ client/api/apiLinks.js
@@ -4,7 +4,7 @@
 // export const chatUrl = "http://localhost:3000/chat/";
 // export const doctorChatUrl = "http://localhost:3000/doctor/chat/";
 
-export const userUrl = "https://www.i-care.tech";
+export const userUrl = "https://www.i-care.tech/";
 export const doctorUrl = "https://www.i-care.tech/doctor/";
 export const adminUrl = "https://www.i-care.tech/admin/";
 export const chatUrl = "https://www.i-care.tech/chat/";
