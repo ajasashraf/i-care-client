0d503b0bb026cae142f6a6041da9f3adddcdb8e7
Ajas Ashraf <ajasashraf7010@gmail.com>
2023-04-27 11:16:37 +0530

    Sales report update

--- client/src/components/AdminComponents/SalesReport/SalesReport.jsx
+++ client/src/components/AdminComponents/SalesReport/SalesReport.jsx
@@ -1,14 +1,26 @@
 import React, { useContext, useEffect, useState } from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarDensitySelector,
+} from "@mui/x-data-grid";
+import { Button } from "@mui/material";
 import axios from "axios";
 import { adminUrl } from "../../../../api/apiLinks";
 import { adminLoading } from "../../../pages/Admin/Home/Home";
 import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import Papa from "papaparse";
+import { saveAs } from "file-saver";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 const SalesReport = () => {
   const [sales, setSales] = useState([]);
-  const [reload] = useState(false);
+  const [reload, setReload] = useState(false);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
   let token = localStorage.getItem("adminToken");
   const Navigate = useNavigate();
   const { changeLoading } = useContext(adminLoading);
@@ -17,7 +29,9 @@ const SalesReport = () => {
     token = localStorage.getItem("adminToken");
     const headers = { authorization: token };
     axios
-      .get(`${adminUrl}getSales`, { headers })
+      .get(`${adminUrl}getSales?startDate=${startDate}&endDate=${endDate}`, {
+        headers,
+      })
       .then((response) => {
         setSales(response.data);
       })
@@ -27,7 +41,17 @@ const SalesReport = () => {
           : toast.error("something went wrong");
       })
       .finally(() => changeLoading(false));
-  }, [reload]);
+  }, [reload, startDate, endDate]);
+
+  const handleDateChange = (e) => {
+    if (e.target.id === "startDate") {
+      setStartDate(e.target.value);
+      document.getElementById("endDate").setAttribute("min", e.target.value);
+    } else if (e.target.id === "endDate") {
+      setEndDate(e.target.value);
+      document.getElementById("startDate").setAttribute("max", e.target.value);
+    }
+  };
 
   const columns = [
     {
@@ -84,9 +108,62 @@ const SalesReport = () => {
     },
   ];
 
+  const CustomToolbar = () => {
+    const handleExport = () => {
+      console.log("herre");
+      // Get the current data from the DataGrid
+      const data = sales.map((row) => ({
+        ...row,
+        patientId: row.patientId ? row.patientId.fullName : "",
+        doctorId: row.doctorId ? `Dr ${row.doctorId.fullName}` : "",
+      }));
+
+      // Convert the data to CSV format
+      const csv = new Blob([Papa.unparse(data)], {
+        type: "text/csv;charset=utf-8",
+      });
+
+      // Download the CSV file
+      saveAs(csv, "data.csv");
+    };
+    return (
+      <div className="w-full flex justify-end mb-2">
+        <GridToolbarContainer>
+          <GridToolbarColumnsButton />
+          <GridToolbarFilterButton />
+          <GridToolbarDensitySelector className="me-2" />
+          <Button
+            onClick={handleExport}
+            variant="text"
+            color="inherit"
+            size="small"
+            startIcon={<ArrowDownwardIcon />}
+            className="text-primary "
+          >
+            export
+          </Button>
+        </GridToolbarContainer>
+      </div>
+    );
+  };
+
   return (
     <div className="dataTable w-100">
       <Toaster />
+      <div className="flex mb-2 w-full justify-end ">
+        <input
+          type="date"
+          id="startDate"
+          className="me-2 rounded-md border border-gray-300 p-2"
+          onChange={handleDateChange}
+        />
+        <input
+          type="date"
+          id="endDate"
+          className="rounded-md border border-gray-300 p-2 me-2"
+          onChange={handleDateChange}
+        />
+      </div>
       <DataGrid
         getRowId={(row) => row._id}
         rows={sales}
@@ -94,6 +171,7 @@ const SalesReport = () => {
         pageSize={10}
         rowsPerPageOptions={[10]}
         disableSelectionOnClick
+        components={{ Toolbar: CustomToolbar }}
       />
     </div>
   );
