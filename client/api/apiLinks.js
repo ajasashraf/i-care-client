2ac6240301796796aad3ee4b259b14167c82ae9d
Ajas Ashraf <ajasashraf7010@gmail.com>
2023-06-27 12:58:40 +0530

    commit

--- client/api/apiLinks.js
+++ client/api/apiLinks.js
@@ -5,7 +5,7 @@
 // export const doctorChatUrl = "http://localhost:2000/doctor/chat/";
 
 export const userUrl = "https://icare-backend-gn76.onrender.com/";
-export const doctorUrl = "hhttps://icare-backend-gn76.onrender.com/doctor/";
+export const doctorUrl = "https://icare-backend-gn76.onrender.com/doctor/";
 export const adminUrl = "https://icare-backend-gn76.onrender.com/admin/";
 export const chatUrl = "https://icare-backend-gn76.onrender.com/chat/";
 export const doctorChatUrl = "https://icare-backend-gn76.onrender.com/chat/";
